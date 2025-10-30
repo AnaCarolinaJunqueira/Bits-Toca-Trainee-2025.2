88780a43fcbaa35d44896593a0a9a363b7e8b0f1
Angélica <angelica07coutinho@gmail.com>
2025-10-30 08:55:31 -0300

    ajustes formatação

--- public/js/individual_post.js
+++ public/js/individual_post.js
@@ -2,6 +2,7 @@ const likes = document.querySelector('.likes-content');
 
 likes.addEventListener('click', () => {
     likes.classList.toggle('active');
+    // document.querySelector('.likes-text p').textContent = likes.classList.contains('active') ? '01' : '00';
     document.querySelector('.likes-icon i').classList.toggle('bi-heart-fill');
     document.querySelector('.likes-icon i').classList.toggle('bi-heart');
 });
