1b9bf49abe43af3b31ace950200101a9102e1e7b
bruno-keil <bruno.keil@estudante.ufjf.br>
2025-11-11 09:14:52 -0300

    Modais Usuários

--- public/js/listausuarios.js
+++ public/js/listausuarios.js
@@ -97,3 +97,17 @@ function initializeModalLogic() {
 document.addEventListener('DOMContentLoaded', () => {
     initializeModalLogic();
 });
+
+function togglePassword(icon) {
+    const input = icon.previousElementSibling;
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+    } else {
+        input.type = "password";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    }
+}
